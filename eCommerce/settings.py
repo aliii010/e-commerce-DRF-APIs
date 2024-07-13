88fd561646a6ab06e0b1ab19b41fdb7e6915823f
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2024-07-14 02:08:35 +0300

    2nd commit

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -200,7 +200,7 @@ AUTH_USER_MODEL = "users.UserAccount"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'alimajeed8110@gmail.com'
-EMAIL_HOST_PASSWORD = 'qylacrlwrcnnwfwp'
+EMAIL_HOST_USER = getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
